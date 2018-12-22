a5afff3e796624f5c05b4af4a955bf928ea9acd6
Lucas Fernandes Vieira <lucasfv@protonmail.com>
2018-12-22 11:52:19 -0200

    Simplified the build script

--- build.sh
+++ build.sh
@@ -1,12 +1,2 @@
-mkdir build &&
-gcc -c main.cpp -o build/main.o &&
-gcc -c alu.cpp -o build/alu.o &&
-gcc -c fpu.cpp -o build/fpu.o &&
-gcc -c mem.cpp -o build/mem.o &&
-gcc -c gui.cpp -o build/gui.o &&
-gcc -c thread.cpp -o build/thread.o &&
-gcc -c chronometer.cpp -o build/chronometer.o &&
-gcc -c simplebench.cpp -o build/simplebench.o &&
-gcc -o simplebench build/*.o -pthread &&
-rm -r build &&
+gcc -pthread *.cpp -o simplebench &&
 chmod +x simplebench
