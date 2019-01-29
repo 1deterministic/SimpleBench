7053b37deed7b497512fd7a1c306b2b21c6c9905
Lucas Fernandes Vieira <22532656+1deterministic@users.noreply.github.com>
2019-01-29 01:27:34 -0200

    Added comments to get_cli_options

--- main.cpp
+++ main.cpp
@@ -58,99 +58,137 @@ int main(int argc, char** argv) {
 // reads the options received via CLI
 MsgCode get_cli_options(int argc, char** argv, bool* show_gui, bool* st_test, bool* mt_test, bool* old_hardware, int* threads) {
     for (int i = 1; i < argc; i++) {
+        // --show-gui
         if (strcmp(argv[i], CLI_SHOW_GUI) == 0) {
+            // check if the next argument exists before trying to access
             if (i + 1 < argc) {
                 i++;
+                // if the option is "on"
                 if (strcmp(argv[i], CLI_ON) == 0) {
                     *show_gui = true;
                 }
+                // if the option is "off"
                 else if (strcmp(argv[i], CLI_OFF) == 0) {
                     *show_gui = false;
                 }
+                // next argument is not "on"/"off"
                 else {
                     return MSG_GET_CLI_OPTIONS_INVALID_ONOFF;
                 }
+                // no need to test the other if's, goes to the next argument
                 continue;
             } else {
+                // did not find "on"/"off", reports the error
                 return MSG_GET_CLI_OPTIONS_MISSING_ONOFF;
             }
         }
 
+        // --st-test
         if (strcmp(argv[i], CLI_ST_TEST) == 0) {
+            // check if the next argument exists before trying to access
             if (i + 1 < argc) {
                 i++;
+                // if the option is "on"
                 if (strcmp(argv[i], CLI_ON) == 0) {
                     *st_test = true;
                 }
+                // if the option is "off"
                 else if (strcmp(argv[i], CLI_OFF) == 0) {
                     *st_test = false;
                 }
+                // next argument is not "on"/"off"
                 else {
                     return MSG_GET_CLI_OPTIONS_INVALID_ONOFF;
                 }
+                // no need to test the other if's, goes to the next argument
                 continue;
             } else {
+                // did not find "on"/"off", reports the error
                 return MSG_GET_CLI_OPTIONS_MISSING_ONOFF;
             }
         }
 
+        // --mt-test
         if (strcmp(argv[i], CLI_MT_TEST) == 0) {
+            // check if the next argument exists before trying to access
             if (i + 1 < argc) {
                 i++;
+                // if the option is "on"
                 if (strcmp(argv[i], CLI_ON) == 0) {
                     *mt_test = true;
                 }
+                // if the option is "off"
                 else if (strcmp(argv[i], CLI_OFF) == 0) {
                     *mt_test = false;
                 }
+                // next argument is not "on"/"off"
                 else {
                     return MSG_GET_CLI_OPTIONS_INVALID_ONOFF;
                 }
+                // no need to test the other if's, goes to the next argument
                 continue;
             } else {
+                // did not find "on"/"off", reports the error
                 return MSG_GET_CLI_OPTIONS_MISSING_ONOFF;
             }
         }
 
+        // --threads
         else if (strcmp(argv[i], CLI_THREADS) == 0) {
+            // check if the next argument exists before trying to access
             if (i + 1 < argc) {
                 i++;
+                // converts the text to an integer value
                 *threads = atoi(argv[i]);
+                // only values greater than zero are allowed
+                // also, atoi will return 0 if the text is not an integer
                 if (*threads <= 0) {
                     return MSG_GET_CLI_OPTIONS_INVALID_INT;
-                }    
+                }   
+                // no need to test the other if's, goes to the next argument 
                 continue;
             } else {
+                // did not find a valid value, reports the error
                 return MSG_GET_CLI_OPTIONS_MISSING_INT;
             }
         }
 
+        // --old-hardware
         if (strcmp(argv[i], CLI_OLD_HARDWARE) == 0) {
+            // check if the next argument exists before trying to access
             if (i + 1 < argc) {
                 i++;
+                // if the option is "on"
                 if (strcmp(argv[i], CLI_ON) == 0) {
                     *old_hardware = true;
                 }
+                // if the option is "off"
                 else if (strcmp(argv[i], CLI_OFF) == 0) {
                     *old_hardware = false;
                 }
+                // next argument is not "on"/"off"
                 else {
                     return MSG_GET_CLI_OPTIONS_INVALID_ONOFF;
                 }
+                // no need to test the other if's, goes to the next argument
                 continue;
             } else {
+                // did not find "on"/"off", reports the error
                 return MSG_GET_CLI_OPTIONS_MISSING_ONOFF;
             }
         }
 
+        // --version
         else if (strcmp(argv[i], CLI_VERSION) == 0) {
             return MSG_GET_CLI_OPTIONS_VERSION;
         }
 
+        // --help
         else if (strcmp(argv[i], CLI_HELP) == 0) {            
             return MSG_HELP_TEXT;
         }
 
+        // not recognized option
         else {
             return MSG_GET_CLI_OPTIONS_UNKNOWN_OPTION;
         }
