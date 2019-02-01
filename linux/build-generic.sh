d29aa1807cbcb66f1b99ba991f42636c66889620
Lucas Fernandes Vieira <22532656+1deterministic@users.noreply.github.com>
2019-02-01 01:41:16 -0200

    Removed old code

--- linux/build-generic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gcc -O0 -m32 -pthread *.c -o simplebench32 &&
-chmod +x simplebench32 &&
-gcc -O0 -pthread *.c -o simplebench &&
-chmod +x simplebench &&
-echo "Done!"
--- linux/build-native.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -O0 -march=native -pthread *.c -o simplebench &&
-chmod +x simplebench &&
-echo "Done!"
