6cda9384199b3f157cff82278efc88b134495929
Lucas Fernandes <22532656+1deterministic@users.noreply.github.com>
2019-03-11 09:52:31 -0300

    Improved the readability of the build scripts

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# 32-bit
 gcc -O0 -m32 -pthread *.c -o simplebench32-linux -lm &&
 chmod +x simplebench32-linux &&
+# 64-bit
 gcc -O0 -pthread *.c -o simplebench-linux -lm &&
 chmod +x simplebench-linux &&
+
 echo "Done!"
--- build-windows.sh
+++ build-windows.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# 32-bit
+i686-w64-mingw32-gcc -O0 *.c -o simplebench32-windows.exe &&
+# 64-bit
 x86_64-w64-mingw32-gcc -O0 *.c -o simplebench-windows.exe &&
-i686-w64-mingw32-gcc -O0 *.c -o simplebench32-windows.exe
+
+echo "Done!"
